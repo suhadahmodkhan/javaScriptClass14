e8fa457890004a90d53b630e051876278dc1a40c
suhadahmodkhan <suhadahmodkhan04@gmail.com>
2024-08-17 01:11:11 -0700

    console

---
+++ app.js
@@ -0,0 +1,254 @@
+const mainArray = ["abcd","abc" ]
+// console.log(array)
+const reverse = mainArray.reverse()
+const sort = mainArray.toSorted()
+// console.log( array)
+// console.log(sort)
+
+// includes 
+let jatio = "hello world"
+const includes = jatio.includes("p")
+// console.log(includes)
+
+const numberArray = [
+    
+    {
+      "_id": "66a384e93b2821fd32adcfcb",
+      "fullName": "Ananna Akther ",
+      "email": "biyasadi2032@gmail.com",
+      "mobile": "01746778896",
+      "image": "https://i.ibb.co/5sLpkTq/wp4846536.jpg",
+      "role": "user"
+    },
+    {
+      "_id": "66a387521bf386f68ad4c066",
+      "fullName": "Ruhina Begum",
+      "email": "biyasadisDDDDylheti@gmail.com",
+      "mobile": "+8801778297395",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a388891bf386f68ad4c068",
+      "fullName": "Farhana Begum ",
+      "email": "biyasadggfdisylheti@gmail.com",
+      "mobile": "+8801751819772",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a38ad1a6bef787da800d2d",
+      "fullName": "Tahera Akther ",
+      "email": "biyasadffgsfcisylheti@gmail.com",
+      "mobile": "+8801770015675",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a38c13a6bef787da800d30",
+      "fullName": "Syeda Saumy Begum ",
+      "email": "biyasadisylgdgheti@gmail.com",
+      "mobile": "+8801712917048",
+      "image": "https://i.ibb.co/7QJxZ7y/wal.jpg",
+      "role": "user"
+    },
+    {
+      "_id": "66a38d402f690fb325bf4d45",
+      "fullName": "Farzana Akther ",
+      "email": "biyasadisyghdylheti@gmail.com",
+      "mobile": "No Entry",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a38e30a0d7a5011887f901",
+      "fullName": "Sayeda Nadia Islam ",
+      "email": "biyasadisnvyjylheti@gmail.com",
+      "mobile": "media kudrot",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a38e36a0d7a5011887f904",
+      "fullName": "Sayeda Nadia Islam ",
+      "email": "biyasadisnvyjylheti@gmail.com",
+      "mobile": "media kudrot",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a3923387a719ea20b22d6a",
+      "fullName": "Mohammad Sojib",
+      "email": "biyasadisylhedghti@gmail.com",
+      "mobile": "+8801300589043",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a39704c365493ebeb638b3",
+      "fullName": "Asraf Bokth Manna",
+      "email": "biyasadivnhvnsylheti@gmail.com",
+      "mobile": "+33753325274",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a4a2be9876987f364b0457",
+      "fullName": "Salauddin Sagor Talukdar",
+      "email": "biyasadisdfffsylheti@gmail.com",
+      "mobile": "+8801751162279",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a4a4d805e851015ecdf078",
+      "fullName": "Sakil Ahmed ",
+      "email": "biyasadisylhdfdfdfseti@gmail.com",
+      "mobile": "belal",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a4a61a7f9a91651986bb64",
+      "fullName": "Manik Miah ",
+      "email": "biyasadisyfgfgflheti@gmail.com",
+      "mobile": "Media kudrot ",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a4a789ea5ce20b43d0baa5",
+      "fullName": "Adiluzzaman Nipon ",
+      "email": "biyasadisyccffcvclheti@gmail.com",
+      "mobile": "Media kudrot ali",
+      "image": "https://i.ibb.co/yVdXwcn/IMG-20240717-WA0008.jpg",
+      "role": "user"
+    },
+    {
+      "_id": "66a4a8fbf938086306dd09de",
+      "fullName": "Abir Hannan Chowdhury",
+      "email": "biyasadisylhdtdttheti@gmail.com",
+      "mobile": "kud media",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a4abd4b7e5c5a9e9e0db97",
+      "fullName": "Aminur Rahman ",
+      "email": "biyasadisylhDFGDFGeti@gmail.com",
+      "mobile": "+447849872474",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a4ad3b1b8c14b115c7cf39",
+      "fullName": "Abu Bokkor ",
+      "email": "biyasadisylasasaheti@gmail.com",
+      "mobile": "+601111509087",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a4b00cb5678a0843b8a1a8",
+      "fullName": "Mosahid Miah ",
+      "email": "biyasadisylhfvfvveti@gmail.com",
+      "mobile": "+8801746118629",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a4b1ce8f49436efd795602",
+      "fullName": "Mamun Hosen ",
+      "email": "biyasadisydrdcgheti@gmail.com",
+      "mobile": "+8801747881731",
+      "image": "",
+      "role": "user"
+    },
+    {
+      "_id": "66a8211a6a2dc3f238d6e7c3",
+      "fullName": "suhad ahmod khan",
+      "email": "suhadahmodkhan04@gmail.com",
+      "mobile": "01837102187",
+      "image": "https://i.ibb.co/PWkNC4v/extra.jpg",
+      "role": "user"
+    },
+    {
+      "_id": "66acc3d7fe233f55fd2fca80",
+      "fullName": "Fatema Begum",
+      "email": "biyasadisylhdtdthheti@gmail.com",
+      "mobile": "+8801722299649",
+      "image": "https://i.ibb.co/wYRBt5H/IMG-20240609-234625-832-1.jpg",
+      "role": "user"
+    },
+    {
+      "_id": "66acc4c1fe233f55fd2fca82",
+      "fullName": "Ruhena Akther Ruma ",
+      "email": "takbirgfci393@gmail.com",
+      "mobile": "+8801724689361",
+      "image": "https://i.ibb.co/3yH7FZf/photo-1525373698358-041e3a460346.jpg",
+      "role": "user"
+    },
+    {
+      "_id": "66acc5cb69819179dbe62ded",
+      "fullName": "Tamanna Islam Iti",
+      "email": "takbiri39ghdgh3@gmail.com",
+      "mobile": "+8801647083808",
+      "image": "https://i.ibb.co/W0YVT5z/Whats-App-Image-2024-06-25-at-02-04-52-000bb0ba.jpg",
+      "role": "user"
+    },
+    {
+      "_id": "66acc6ff69819179dbe62def",
+      "fullName": "Runa Begum",
+      "email": "takbirgfci3rgr93@gmail.com",
+      "mobile": "+8801836872785",
+      "image": "https://i.ibb.co/W0YVT5z/Whats-App-Image-2024-06-25-at-02-04-52-000bb0ba.jpg",
+      "role": "user"
+    },
+    {
+      "_id": "66acc8e11bd6c58feabe7539",
+      "fullName": "Parvin Akther ",
+      "email": "takbiri39gfgbgfbvhdgh3@gmail.com",
+      "mobile": "+8801781585912",
+      "image": "https://i.ibb.co/W0YVT5z/Whats-App-Image-2024-06-25-at-02-04-52-000bb0ba.jpg",
+      "role": "user"
+    },
+    {
+      "_id": "66acca5d4d786f8f2f53ecfa",
+      "fullName": "Tayoba Akther Mukta ",
+      "email": "biyasadisydsdfslchggchchghgchdtdthheti@gmail.com",
+      "mobile": "juber",
+      "image": "https://i.ibb.co/W0YVT5z/Whats-App-Image-2024-06-25-at-02-04-52-000bb0ba.jpg",
+      "role": "user"
+    }
+  ]
+const newArray = []
+for(let i = 0 ; i < numberArray.length ; i++){
+    let number = numberArray[i] * 2 ;
+    newArray.push(number)
+}
+// console.log(newArray)
+
+const arrayMap = numberArray.map((num) => num * 2)
+// console.log(arrayMap)
+
+const filter = numberArray.filter((num) => num.role === "admin")
+// console.log(filter)
+
+const find = numberArray.find((num) => num.role === "admin" )
+// console.log(find)
+const numArray = [1,2,3,4]
+const reduce = numArray.reduce((prev,current) => current - prev)
+// console.log(Math.abs(reduce))
+
+const some = numberArray.some((num) => num.role === "admin")
+
+const every = numberArray.every((num) => num.role === "user")
+
+
+function functionName() {
+
+}
+
+const functionName = () => {
+    
+}
